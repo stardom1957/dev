841829af9c7f205187a4c1db2ba7ae8e692109fc
Guy Nicol <105818788+GuyNicol@users.noreply.github.com>
2022-11-18 15:51:19 -0500

    Update primes_GN.py
    Test de modification pour notre apprentissage de GitHub.
    Ajustement des commentaires.

--- python/primes_GN.py
+++ python/primes_GN.py
@@ -4,16 +4,17 @@
 #  le Raspberry Pi
 # Reçu:2022-10-07 de Stardom
 # C:2022-10-07 : M:2022-10-09
-# primes.py version 1.1a
+# primes.py version 1.1b
 # Remplacement des prints par l'utilisation d'une liste
 # Ajout de commentaires expliquant le fonctionnement
+# Correction de l'indentation
 #!/usr/bin/env python3
 n=int(input("Quel est le nombre maximum désiré ? "))
 sieve=set(range(2,n+1))
 liste=[]
 while sieve:					# Tant que l'ensemble n'est pas vide
-    prime=min(sieve)				# Obtenir la valeur du plus petit élément
-    liste.append(prime)				# Ajouter le nouveau nombre premier dans la liste
+    prime=min(sieve)			# Obtenir la valeur du plus petit élément
+    liste.append(prime)			# Ajouter le nouveau nombre premier dans la liste
     sieve-=set(range(prime, n+1, prime))	# Enlever de l'ensemble cet élément et tous ses multiples
  						# Recommencer ...
 print(liste)
